fc12406efc25b4cc903598234770d6a5522a5c14
elch93 <eugeneliong@hotmail.com>
2020-04-26 10:27:00 +0800

    added redirection to user page after registration

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-import os 
-from flask import Flask, redirect, render_template, url_for, request
+import os
+from flask import Flask, redirect, render_template, url_for, request, flash
 from dotenv import load_dotenv
 import pymongo
-import flask_login  #for handling logins/logouts
-from passlib.hash import pbkdf2_sha256  #for encrypting password
+import flask_login  # for handling logins/logouts
+from passlib.hash import pbkdf2_sha256  # for encrypting password
 
-#load env file
+# load env file
 load_dotenv()
 
 app = Flask(__name__)
@@ -21,8 +21,11 @@ login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
 
 # create user
+
+
 class User(flask_login.UserMixin):
     pass
+
 
 @login_manager.user_loader
 def user_loader(email):
@@ -35,10 +38,14 @@ def user_loader(email):
     return logged_in_user
 
 # encrypt user's password
+
+
 def password_encryptor(user_password):
     return pbkdf2_sha256.hash(user_password)
 
 # verify user's password
+
+
 def verify_password(user_input, encrypted_password):
     return pbkdf2_sha256.verify(user_input, encrypted_password)
 
@@ -52,31 +59,43 @@ def index():
 @app.route('/', methods=["POST"])
 def process_input():
     # user is trying to sign up
-    if request.form.get('registeremail') and request.form.get('registerpw'):
-        create_email, create_pw = request.form.get('registeremail'), request.form.get('registerpw')
+    if request.form.get('registeremail') and request.form.get('registerpw') and request.form.get('registername'):
+        create_email, create_pw, create_dname = request.form.get(
+            'registeremail'), request.form.get('registerpw'), request.form.get('registername')
         # first check if email already exists in database
         if not client[dbname]['registered_users'].find_one({
             "email": create_email
         }):
-        # if not found, create an account
-            client[dbname]['registered_users'].insert({
+            # if not found, create an account
+            client[dbname]['registered_users'].insert_one({
+                "displayname": create_dname,
                 "email": create_email,
                 "password": password_encryptor(create_pw)
             })
 
-            return "USER CREATED"
-            
+            # then allow the user to login
+            logged_in_user = User()
+            user_data = client[dbname]['registered_users'].find_one({
+                "email": create_email
+            })
+
+            logged_in_user.id = user_data['email']
+            flask_login.login_user(logged_in_user)
+            return render_template('index.template.html', state="default")
+
         else:
-        # if found, prevent creation
-            return "USER ALREADY EXISTS"
-            
+            # if found, prevent creation
+            flash(create_email + ' is already in use. Please try again.')
+            return redirect(url_for('index'))
 
     # user is trying to login
     if request.form.get('loginemail') and request.form.get('loginpw'):
-        login_email, login_pw = request.form.get('loginemail'), request.form.get('loginpw')
+        login_email, login_pw = request.form.get(
+            'loginemail'), request.form.get('loginpw')
         if client[dbname]['registered_users'].find_one({"email": login_email}):
             # email exists, check pw now
-            user_data = client[dbname]['registered_users'].find_one({"email": login_email})
+            user_data = client[dbname]['registered_users'].find_one(
+                {"email": login_email})
             if verify_password(login_pw, user_data['password']):
                 logged_in_user = User()
                 logged_in_user.id = user_data['email']
@@ -85,8 +104,8 @@ def process_input():
             else:
                 return "USER FOUND BUT WRONG PASSWORD"
         else:
-            return "USER NOT FOUND" 
-    
+            return "USER NOT FOUND"
+
     # create note
     if request.form.get('editordata'):
         created_note = request.form.get('editordata')
@@ -114,27 +133,14 @@ def logout():
 def test():
     results = client.sample_proj3.studynotes.find()
     print(results)
-    return render_template('test.template.html', results = results)
-
-
-
-
-
-
-
-
-
-
-
-
+    return render_template('test.template.html', results=results)
 
 
 
 if __name__ == '__main__':
     app.run(
-        host = os.environ.get('IP'),
-        port = os.environ.get('PORT'),
-        debug = True
+        host=os.environ.get('IP'),
+        port=os.environ.get('PORT'),
+        debug=True
     )
-
 
