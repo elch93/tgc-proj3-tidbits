56018a204204e641f61ccd336cc2ff730128353c
elch93 <eugeneliong@hotmail.com>
2020-05-04 10:51:18 +0800

    removed non-existent id from login submit

--- app.py
+++ app.py
@@ -123,8 +123,7 @@ def process_input():
                 return redirect(url_for('search'))
             else:
                 # if display name is used, prevent creation
-                myalert = 'The display name ' + create_dname + \
-                    ' is already in use. Please try again.'
+                myalert = 'The display name ' + create_dname + ' is already in use. Please try again.'
                 return render_template('index.template.html', myalert=myalert)
 
         else:
@@ -153,6 +152,8 @@ def process_input():
         else:
             myalert = 'Email is wrong. Please try again.'
             return render_template('index.template.html', myalert=myalert)
+    else:
+        return ('', 204)
 
 # create note page
 @app.route('/create', methods=['GET', 'POST'])
