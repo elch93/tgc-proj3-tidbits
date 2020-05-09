218867a094fc6b1bf755ffe2f1605f8bfe23e254
elch93 <eugeneliong@hotmail.com>
2020-05-10 02:29:15 +0800

    updated tool bar with logout link

--- static/script.js
+++ static/script.js
@@ -78,25 +78,25 @@ $(function () {
     $('#loginpanel').find('h3').eq(0).css('border-bottom', '#FBB03B 5px solid');
 
     // sprites appear on hover
-    $('#toolbar a').eq(0).hover(function(){
+    $('#toolbar a').eq(1).hover(function(){
         $('#createsprite').css('transform','translateY(-50px)')
     },function(){
         $('#createsprite').css('transform','translateY(0px)')
     })
 
-    $('#toolbar a').eq(1).hover(function(){
+    $('#toolbar a').eq(2).hover(function(){
         $('#searchsprite').css('transform','translateY(-50px)')
     },function(){
         $('#searchsprite').css('transform','translateY(0px)')
     })
 
-    $('#toolbar a').eq(2).hover(function(){
+    $('#toolbar a').eq(3).hover(function(){
         $('#notesprite').css('transform','translateY(-50px)')
     },function(){
         $('#notesprite').css('transform','translateY(0px)')
     })
 
-    $('#toolbar a').eq(3).hover(function(){
+    $('#toolbar a').eq(4).hover(function(){
         $('#likesprite').css('transform','translateY(-50px)')
     },function(){
         $('#likesprite').css('transform','translateY(0px)')
