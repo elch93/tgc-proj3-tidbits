ead6cf694547f40c91152bad228e0f4edecef347
elch93 <eugeneliong@hotmail.com>
2020-05-10 20:51:33 +0800

    restored font style option to summernote

--- static/script.js
+++ static/script.js
@@ -142,7 +142,7 @@ $(function () {
         width: 700,
         toolbar: [
             // [groupName, [list of button]]
-            ['style', ['bold', 'italic', 'underline', 'clear']],
+            ['style', ['style', 'bold', 'italic', 'underline', 'clear']],
             ['font', ['strikethrough', 'superscript', 'subscript']],
             ['fontsize', ['fontsize']],
             ['color', ['color']],
